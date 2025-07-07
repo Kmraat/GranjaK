ef7856f81322b8939ff696bd1f9a49a55fca645d
Kmraat <km_cm23@hotmail.com>
2025-07-07 14:51:22 -0600

    v3

--- MiGranjaKM/Assets/Scripts/MovimientoJugador.cs
+++ MiGranjaKM/Assets/Scripts/MovimientoJugador.cs
@@ -6,10 +6,12 @@ public class MovimientoJugador : MonoBehaviour
     public float velocidad = 5f;
    public Rigidbody2D  rg; //rigidbody
         public Vector2 entrada;
+    Animator animator;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
  void Start()
     {
      rg = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         
     }
 
@@ -22,6 +24,35 @@ public class MovimientoJugador : MonoBehaviour
 
     public void Moverse(InputAction.CallbackContext contexto)
     {
-        entrada = contexto.ReadValue<Vector2>();
+
+        animator.SetBool("estaCaminando", true);
+
+
+        Vector2 valorEntrada = contexto.ReadValue<Vector2>();
+
+        // Determinar el eje dominante
+        if (Mathf.Abs(valorEntrada.x) > Mathf.Abs(valorEntrada.y))
+        {
+            // Movimiento horizontal
+            entrada = new Vector2(Mathf.Sign(valorEntrada.x), 0);
+        }
+        else if (Mathf.Abs(valorEntrada.y) > 0)
+        {
+            // Movimiento vertical
+            entrada = new Vector2(0, Mathf.Sign(valorEntrada.y));
+        }
+        else
+        {
+            entrada = Vector2.zero;
+        }
+
+        animator.SetFloat("entradaX", entrada.x);
+        animator.SetFloat("entradaY", entrada.y);
+
+        if (contexto.canceled)
+        {
+            animator.SetBool("estaCaminando", false);
+        }
+        
     }
 }
