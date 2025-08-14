b296746b83927bf25cb9fc69dbdda0d3e6f7e717
Kmraat <km_cm23@hotmail.com>
2025-08-14 00:42:09 -0600

    resubiendoarchivos

---
+++ MiGranjaKM/Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+   public int ConteoHuevos;
+    public static GameManager instancia;
+    
+    void Awake()
+    {
+        if (instancia == null)
+        {
+            instancia = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+    public void SumarHuevo()
+    {
+        ConteoHuevos++;
+        Debug.Log("ConteoHuevos");
+    }
+
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
